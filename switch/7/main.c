ae4e55f65ac9784ec0f2756367b9e0875ab37506
Roaa8 <139008877+Roaa8@users.noreply.github.com>
2023-08-05 22:29:43 +0300

    Add files via upload

---
+++ switch/7/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a, b, c;
+    float root1, root2, imaginary;
+    float discriminant;
+
+    printf("Enter values of a, b, c of quadratic equation (aX^2 + bX + c): ");
+    scanf("%f%f%f", &a, &b, &c);
+
+    discriminant = (b * b) - (4 * a * c);
+
+
+    switch(discriminant > 0)
+    {
+        case 1:
+            root1 = (-b + sqrt(discriminant)) / (2 * a);
+            root2 = (-b - sqrt(discriminant)) / (2 * a);
+
+            printf("Two distinct and real roots exists: %f and %f",
+                    root1, root2);
+            break;
+
+        case 0:
+            switch(discriminant < 0)
+            {
+                case 1:
+                    root1 = root2 = -b / (2 * a);
+                    imaginary = sqrt(-discriminant) / (2 * a);
+
+                    printf("Two distinct complex roots exists: %f + i%f and %f - i%f",
+                            root1, imaginary, root2, imaginary);
+                    break;
+
+                case 0:
+                    root1 = root2 = -b / (2 * a);
+
+                    printf("Two equal and real roots exists: %f and %f", root1, root2);
+
+                    break;
+            }
+    }
+    return 0;
+}
---
+++ switch/8/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     char op;
+    float num1, num2, result=0.0f;
+
+    printf("WELCOME TO THE CALCULATOR\n");
+    printf("----------------------------\n");
+    printf("Enter [number 1] [+ - * /] [number 2]\n");
+
+    scanf("%f %c %f", &num1, &op, &num2);
+
+    switch(op)
+    {
+        case '+':
+            result = num1 + num2;
+            break;
+
+        case '-':
+            result = num1 - num2;
+            break;
+
+        case '*':
+            result = num1 * num2;
+            break;
+
+        case '/':
+            result = num1 / num2;
+            break;
+
+        default:
+            printf("Invalid operator");
+    }
+
+    printf("%f %c %f = %f", num1, op, num2, result);
+
+    return 0;
+}
